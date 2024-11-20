09eaea97d811bd29c5bd7104f45d662b99fbdaff
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2024-11-20 12:27:56 +0000

    Documenting the scripts

--- matflow/data/scripts/damask/viz_damask_HDF5.py
+++ matflow/data/scripts/damask/viz_damask_HDF5.py
@@ -1,12 +1,12 @@
+from __future__ import annotations
 from pathlib import Path
-from typing import Dict, List, Union
 
 from damask_parse.utils import generate_viz
 
 
 def viz_damask_HDF5(
-    damask_hdf5_file: Path,
-    damask_viz: Union[Dict, List],
-    VE_response: Dict,
-):
+    damask_hdf5_file: Path | str,
+    damask_viz: dict | list,
+    VE_response: dict,
+) -> None:
     generate_viz(hdf5_path=damask_hdf5_file, viz_spec=damask_viz, parsed_outs=VE_response)
--- matflow/data/scripts/damask/write_load.py
+++ matflow/data/scripts/damask/write_load.py
@@ -1,8 +1,26 @@
+from __future__ import annotations
+from pathlib import Path
+from typing import TYPE_CHECKING
 from damask import __version__
 from damask_parse.writers import write_load_case
+if TYPE_CHECKING:
+    from matflow.param_classes.load import LoadCase
 
 
-def write_load(path, load_case, damask_solver):
+def write_load(path: Path | str, load_case: LoadCase, damask_solver: dict[str, str] | None) -> None:
+    """Write the load file for a DAMASK simulation.
+
+    Parameters
+    ----------
+    path
+        Full path to load file to write.
+    load_case
+        The loadings to write.
+    solver
+        Override information about the solver to use.
+    """
+    path_ = Path(path)
+
     load_steps = []
     for step in load_case.steps:
         dct = step.to_dict()
@@ -11,9 +29,9 @@ def write_load(path, load_case, damask_solver):
         load_steps.append(dct)
 
     write_load_case(
-        dir_path=path.parent,
+        dir_path=path_.parent,
         load_cases=load_steps,
         solver=damask_solver,
-        name=path.name,
+        name=path_.name,
         write_2D_arrs=(__version__ != "3.0.0-alpha3"),
     )
