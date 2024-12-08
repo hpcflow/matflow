1210a842a2c4ab249181533c21a36b34f54e76e8
Adam <adam.jp@live.co.uk>
2024-12-08 13:52:02 +0000

    refactor: reuse util function from hpcflow

--- matflow/tests/utils.py
+++ matflow/tests/utils.py
@@ -1,19 +1,19 @@
 """Utility functions to assist testing."""
 
-from importlib import resources
+from functools import partial
+
+from hpcflow.sdk.core.test_utils import (
+    make_test_data_YAML_workflow,
+    make_test_data_YAML_workflow_template,
+)
 
 import matflow as mf
 
 
-def make_test_data_YAML_workflow(workflow_name, path, **kwargs):
-    """Generate a workflow whose template file is defined in the test data directory."""
-    # TODO: re-use this function in hpcflow, rather than defining it again here.
-    pkg = "matflow.tests.data"
-    try:
-        script_ctx = resources.as_file(resources.files(pkg).joinpath(workflow_name))
-    except AttributeError:
-        # < python 3.9; `resource.path` deprecated since 3.11
-        script_ctx = resources.path(pkg, workflow_name)
+make_test_data_YAML_workflow = partial(
+    make_test_data_YAML_workflow, app=mf, pkg="matflow.tests.data"
+)
 
-    with script_ctx as file_path:
-        return mf.Workflow.from_YAML_file(YAML_path=file_path, path=path, **kwargs)
+make_test_data_YAML_workflow_template = partial(
+    make_test_data_YAML_workflow_template, app=mf, pkg="matflow.tests.data"
+)
