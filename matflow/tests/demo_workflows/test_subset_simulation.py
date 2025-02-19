15205e85e3057704a00fde796c2fe20cf54816b7
Adam <adam.jp@live.co.uk>
2025-02-19 16:23:15 +0000

    fix: subset_simulation parameter flow tests

--- matflow/tests/demo_workflows/test_subset_simulation.py
+++ matflow/tests/demo_workflows/test_subset_simulation.py
@@ -3,8 +3,11 @@ import matflow as mf
 
 
 @pytest.mark.integration
-def test_subset_sim_toy_model_two_level_external_parameter_flow(null_config, tmp_path):
+def test_subset_sim_toy_model_two_level_external_parameter_flow(
+    new_null_config, tmp_path
+):
 
+    # use `new_null_config` because currently these workflows include template components
     wk = mf.make_demo_workflow(
         "subset_simulation_toy_model_two_level_external", path=tmp_path
     )
@@ -193,13 +196,10 @@ def test_subset_sim_toy_model_two_level_external_parameter_flow(null_config, tmp
             )
 
 
-import pytest
-import matflow as mf
-
-
 @pytest.mark.integration
-def test_subset_sim_DAMASK_Mg_two_level_parameter_flow(null_config, tmp_path):
+def test_subset_sim_DAMASK_Mg_two_level_parameter_flow(new_null_config, tmp_path):
 
+    # use `new_null_config` because currently these workflows include template components
     wk = mf.make_demo_workflow("subset_simulation_DAMASK_Mg_two_level", path=tmp_path)
 
     num_elements = wk.tasks.initialise_markov_chains.num_elements
