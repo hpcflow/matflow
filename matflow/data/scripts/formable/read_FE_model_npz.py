0367abebaae26e49625f0f15de40ee20cde6bcc2
hpcflow-actions <hpcflow-actions@users.noreply.github.com>
2025-03-19 16:24:10 +0000

    pre-commit fixes [skip ci]

--- matflow/data/scripts/formable/read_FE_model_npz.py
+++ matflow/data/scripts/formable/read_FE_model_npz.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 
-def format_1D_masked_array(arr, fill_symbol='x'):
-    'Also formats non-masked array.'
+def format_1D_masked_array(arr, fill_symbol="x"):
+    "Also formats non-masked array."
 
-    return [x.item() if not isinstance(x, np.ma.core.MaskedConstant)
-            else fill_symbol for x in arr]
+    return [
+        x.item() if not isinstance(x, np.ma.core.MaskedConstant) else fill_symbol
+        for x in arr
+    ]
 
 
 def read_FE_model_npz(npz_file_path, idx):
@@ -13,11 +15,11 @@ def read_FE_model_npz(npz_file_path, idx):
     Read loading data from a numpy .npz file and format it for use in a matflow workflow
     """
     data = np.load(npz_file_path)
-    num_incs = data['num_incs']
-    inc_size = data['inc_size']
-    inc_size_final = data['inc_size_final']
-    u_sampled_split = data['u_sampled_split']
-    strain_rate = data['strain_rate']
+    num_incs = data["num_incs"]
+    inc_size = data["inc_size"]
+    inc_size_final = data["inc_size_final"]
+    u_sampled_split = data["u_sampled_split"]
+    strain_rate = data["strain_rate"]
     i = idx
 
     load_cases = []
@@ -30,10 +32,12 @@ def read_FE_model_npz(npz_file_path, idx):
             dt = inc_size[inc_size_idx]
         dt = abs(dt) / strain_rate
 
-        load_cases.append({
-            'target_def_grad': u_sampled_split[i, j],
-            'total_time': dt.item(),
-            'num_increments': 1,
-        })
+        load_cases.append(
+            {
+                "target_def_grad": u_sampled_split[i, j],
+                "total_time": dt.item(),
+                "num_increments": 1,
+            }
+        )
 
-    return {"load_case": {"steps":load_cases}}
+    return {"load_case": {"steps": load_cases}}
