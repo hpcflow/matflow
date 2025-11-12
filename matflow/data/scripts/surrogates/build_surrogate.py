feea4716ccf848f2e247d6a9cef004841d06cceb
Adam <adam.jp@live.co.uk>
2025-11-12 23:08:04 +0000

    feat: add fit_surrogate task

--- matflow/data/scripts/surrogates/build_surrogate.py
+++ matflow/data/scripts/surrogates/build_surrogate.py
@@ -7,10 +7,13 @@ def build_surrogate(
     X_train,
     Y_train,
     parameter_names,
-    n_restarts_optimizer=5,
-    cross_validate=True,
-    scoring="r2",
-    normalize_y=True,
+    n_restarts_optimizer,
+    cross_validate,
+    scoring,
+    normalize_y,
+    validate,
+    X_test,
+    Y_test,
 ):
     surrogate = Surrogate(X_train, Y_train, parameter_names=parameter_names, scale=True)
     surrogate.build_model(
@@ -20,4 +23,24 @@ def build_surrogate(
         normalize_y=normalize_y,
     )
 
-    return {"surrogate": surrogate}
+    # validation:
+    vld_out = {}
+    if validate:
+        Y_prediction, Y_error = surrogate.make_prediction(X_test, return_std=True)
+        score = sklearn.metrics.r2_score(Y_test, Y_prediction)
+        print(f"Validation score: {score}.")
+        vld_out = {
+            "Y_prediction": Y_prediction,
+            "Y_error": Y_error,
+            "score": score,
+        }
+
+    # versions: once serialised, the model (e.g. GaussianProcessRegressor) may not be
+    # correctly deserialised when using different versions of scikit-learn and its
+    # dependencies:
+    versions = {
+        "numpy": np.__version__,
+        "sklearn": sklearn.__version__,
+    }
+
+    return {"surrogate": surrogate, "validation": vld_out, "versions": versions}
