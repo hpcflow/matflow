61eca5b692b7ced656984598460f9495eb48f29c
Adam <adam.jp@live.co.uk>
2025-11-24 20:21:25 +0000

    feat: add `MooseInputDeck` parameter value class

--- matflow/data/scripts/moose/write_input.py
+++ matflow/data/scripts/moose/write_input.py
@@ -1,60 +1,28 @@
-from pathlib import Path
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from matflow.param_classes.moose import MooseInputDeck
 
 
-class MooseBlock:
-    TAB = "    "
+def write_input(
+    path,
+    input_deck: MooseInputDeck | None = None,
+    input_deck_variables=None,
+    elasticity=None,
+    _exodus_generation_input_deck: MooseInputDeck | None = None,
+):
 
-    def __init__(self, name, collection, root=False, variables=None, elasticity=None):
-        self.name = name
-        self.root = root
-        self.attributes = {}
-        self.blocks = []
-        self.variables = variables or {}
+    if _exodus_generation_input_deck is not None:
+        # this is just for generating an Exodus file from a Gmsh .msh file
+        # for easier visualisation:
+        input_deck = _exodus_generation_input_deck
 
-        if elasticity and name == "elasticity":
-
-            # TODO: ideally want a "path" rather than "name"
-            collection.update(elasticity)
-            # print(f"MooseBlock: updating collection: {collection=!r}")
-
-        for key, val in collection.items():
-            # if isinstance(val, CommentedMap):
-            if isinstance(val, dict):
-                self.blocks.append(MooseBlock(key, val, elasticity=elasticity))
-                continue
-            self.attributes[key] = val
-
-    def __str__(self) -> str:
-        if self.root:
-            tab = ""
-            txt = ""
-            for key, val in self.variables.items():
-                txt += f"{key} = {val}\n"
-        else:
-            tab = self.TAB
-            txt = f"[{self.name}]\n"
-
-        for key, val in self.attributes.items():
-            txt += f"{tab}{key} = {val}\n"
-        for block in self.blocks:
-            txt += tab
-            txt += str(block).replace("\n", f"\n{tab}")
-            txt = txt[: len(txt) - len(tab)]
-        if not self.root:
-            txt += "[]\n"
-        return txt
-
-    def to_file(self, path: Path):
-        with path.open("wt", newline="\n") as f:
-            f.write(self.__str__())
-
-
-def write_input(path, input_deck, input_deck_variables, elasticity):
-    input_deck = MooseBlock(
-        "root",
-        input_deck,
-        root=True,
-        variables=input_deck_variables,
-        elasticity=elasticity,
-    )
+    if input_deck_variables:
+        input_deck.add_variables(input_deck_variables)
+    if elasticity:
+        input_deck.update(
+            {("Materials", "elasticity", k): v for k, v in elasticity.items()}
+        )
     input_deck.to_file(path)
