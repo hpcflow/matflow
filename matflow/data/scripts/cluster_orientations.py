2e4494724649d0d56b2bb5166245e14aa19a4755
Gerard Capes <gerard.capes@manchester.ac.uk>
2025-12-08 13:54:59 +0000

    Add completed task schema but with hard-coded values
    The demo workflow uses paths on my machine,
    and the clustering regions/values are hard-coded.
    
    I expect these both need updating.

--- matflow/data/scripts/cluster_orientations.py
+++ matflow/data/scripts/cluster_orientations.py
@@ -1,10 +1,39 @@
 import numpy as np
-from damask_parse.utils import validate_volume_element, validate_orientations
+from damask_parse.utils import validate_volume_element
+from subsurface import Shuffle
 
 
-def cluster_orientations(volume_element):
+def cluster_orientations(volume_element, alpha_file_path, gamma_file_path, n_iterations):
 
     # Convert zarr arrays to numpy arrays using existing code
-    new_volume_element = validate_volume_element(volume_element)
+    volume_element = validate_volume_element(volume_element)
 
-    return {"volume_element": new_volume_element}
+    quaternions = volume_element["orientations"]["quaternions"]
+    material_index = volume_element["element_material_idx"]
+
+    # Replace subsets of quaternion values with those from files
+    alpha = np.load(alpha_file_path)
+    random_alpha_subset = alpha[np.random.choice(alpha.shape[0], size=200, replace=False)]
+    quaternions[150:350] = np.array([list(x) for x in random_alpha_subset])
+    gamma = np.load(gamma_file_path)
+    random_gamma_subset = gamma[np.random.choice(gamma.shape[0], size=200, replace=False)]
+    quaternions[500:700] = np.array([list(x) for x in random_gamma_subset])
+    np.random.shuffle(quaternions)
+
+    # Shuffle orientations
+    # orientations_shuffled_vol,misorientation_init = Shuffle(material_index, quaternions,0,exclude=[],minimize=True,return_full=True)
+    orientations_shuffled_vol, misorientation = Shuffle(
+        material_index,
+        quaternions,
+        n_iterations,
+        exclude=[],
+        minimize=True,
+        return_full=True,
+    )
+
+    # Replace quaternions in volume element
+    volume_element["orientations"]["quaternions"] = np.array(
+        [list(x) for x in orientations_shuffled_vol]
+    )
+
+    return {"volume_element": volume_element}
