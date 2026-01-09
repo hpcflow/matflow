29b7a04574a632adc4b6b6b5c081bfd54307e7d0
Adam <adam.jp@live.co.uk>
2026-01-09 21:52:35 +0000

    fix: tests

--- matflow/tests/param_classes/test_load.py
+++ matflow/tests/param_classes/test_load.py
@@ -22,7 +22,7 @@ def test_single_multistep_uniaxial():
     assert lc1 == lc2
 
 
-def test_load_case_yaml_init(null_config, tmp_path: Path, load_case_1: LoadCase):
+def test_load_case_yaml_init(tmp_path: Path, load_case_1: LoadCase):
     wk = make_test_data_YAML_workflow("define_load.yaml", path=tmp_path)
     load_case = wk.tasks.define_load_case.elements[0].inputs.load_case.value
     assert load_case == load_case_1
