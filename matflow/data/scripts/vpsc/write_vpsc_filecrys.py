9b2ffc9a60391ca00a29bff99b59d700a2dedcb3
Gerard Capes <gerard.capes@manchester.ac.uk>
2026-01-29 14:40:55 +0000

    Write out all parameters for filecrys - some are hard coded
    This is intended to be a short-term hack, to be fixed properly later.

--- matflow/data/scripts/vpsc/write_vpsc_filecrys.py
+++ matflow/data/scripts/vpsc/write_vpsc_filecrys.py
@@ -48,15 +48,46 @@ def write_vpsc_filecrys(path, phases):
             stiffness += '{Z} {Z} {Z} {Z} {C_44} {Z}\n'.format(**c_pars)
             stiffness += '{Z} {Z} {Z} {Z} {Z} {C_44}\n'.format(**c_pars)
 
+            slip_systems = ("<110>(111) SLIP\n"
+                            "1  12  1   0                       modex,nsmx,isensex,itwtypex\n"
+                            "1    1   -1      0    1    1     slip (n & b)\n"
+                            "1    1   -1      1    0    1\n"
+                            "1    1   -1      1   -1    0\n"
+                            "1   -1   -1      0    1   -1\n"
+                            "1   -1   -1      1    0    1\n"
+                            "1   -1   -1      1    1    0\n"
+                            "1   -1    1      0    1    1\n"
+                            "1   -1    1      1    0   -1\n"
+                            "1   -1    1      1    1    0\n"
+                            "1    1    1      0    1   -1\n"
+                            "1    1    1      1    0   -1\n"
+                            "1    1    1      1   -1    0\n")
+            plastic = phase['mechanical']['plastic']
+            nrsx = plastic["nrsx"]
+            tau0x = plastic["tau0x"]
+            tau1x = plastic["tau1x"]
+            thet0 = plastic["thet0"]
+            thet1 = plastic["thet1"]
+            hpfac = plastic["hpfac"]
+            hlatex = plastic["hlatex"]
+            parameters = (f"<111>{110} SLIP -------------------------------------------\n"
+                          f"{nrsx}                           nrsx\n"
+                          f"{tau0x}   {tau1x}   {thet0}   {thet1}  {hpfac}        tau0x,tau1x,thet0,thet1, hpfac\n"
+                          f"{hlatex}                             hlatex(1,im),im=1,nmodes)")
+
         else:
             msg = ('`lattice` must be `cubic`, `hexagonal`.')
             raise ValueError(msg)
 
-        slip_modes = phase.get('slip_modes', [])
-        twin_modes = phase.get('twin_modes', [])
+        slip_modes = phase['mechanical']['plastic'].get('slip_modes', [])
+        twin_modes = phase['mechanical']['plastic'].get('twin_modes', [])
         num_sl = len(slip_modes)
         num_tw = len(twin_modes)
         num_modes = num_sl + num_tw
+        constitutive_law = ("*Constitutive law\n"
+                           "0      Voce=0, MTS=1\n"
+                           "1      iratesens (0:rate insensitive, 1:rate sensitive)\n"
+                           "50     grsze --> grain size only matters if HPfactor is non-zero\n")
 
         path_phase = path.parent / f'{name}.sx'
         with path_phase.open(mode='w') as f:
@@ -71,22 +102,25 @@ def write_vpsc_filecrys(path, phases):
             f.write(f'{num_modes}\n')
             f.write(f'{num_modes}\n')
             f.write(' '.join(str(i) for i in range(1, num_modes + 1)) + '\n')
-            for i, mode in enumerate(slip_modes + twin_modes):
-                tw = i >= num_sl
-                num_sys = len(mode['planes'])
-                assert len(mode['planes']) == len(mode['directions'])
-                f.write(f'{mode["name"]}\n')
-                f.write(f'{i+1} {num_sys} {mode["n"]}')
-                if tw:
-                    f.write(' 0\n')
-                    f.write(f'{mode["twsh"]} {mode["isectw"]} '
-                            f'{mode["thres_1"]} {mode["thres_2"]}\n')
-                else:
-                    f.write(' 1\n')
-                    f.write('0. 0 0. 0.\n')
-                f.write(f'{mode["tau_0"]} {mode["tau_1"]} '
-                        f'{mode["theta_0"]} {mode["theta_1"]} '
-                        f'{mode["hpfac"]} {mode["gndfac"]}\n')
-                f.write(' '.join(str(x) for x in mode['h_latent']) + '\n')
-                for pl, dr in zip(mode['planes'], mode['directions']):
-                    f.write(' '.join(str(x) for x in pl + dr) + '\n')
+            f.write(slip_systems)
+#            for i, mode in enumerate(slip_modes + twin_modes):
+#                tw = i >= num_sl
+#                num_sys = len(mode['planes'])
+#                assert len(mode['planes']) == len(mode['directions'])
+#                f.write(f'{mode["name"]}\n')
+#                f.write(f'{i+1} {num_sys} {mode["n"]}')
+#                if tw:
+#                    f.write(' 0\n')
+#                    f.write(f'{mode["twsh"]} {mode["isectw"]} '
+#                            f'{mode["thres_1"]} {mode["thres_2"]}\n')
+#                else:
+#                    f.write(' 1\n')
+#                    f.write('0. 0 0. 0.\n')
+#                f.write(f'{mode["tau_0"]} {mode["tau_1"]} '
+#                        f'{mode["theta_0"]} {mode["theta_1"]} '
+#                        f'{mode["hpfac"]} {mode["gndfac"]}\n')
+#                f.write(' '.join(str(x) for x in mode['h_latent']) + '\n')
+#                for pl, dr in zip(mode['planes'], mode['directions']):
+#                    f.write(' '.join(str(x) for x in pl + dr) + '\n')
+            f.write(constitutive_law)
+            f.write(parameters) 
